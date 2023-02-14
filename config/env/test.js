2d2f773229a00171d354d546e6c6ac5d148bc290
sourav <sourav@parentheses.co.in>
2023-02-14 11:22:00 +0530

    Changed db name

--- config/env/test.js
+++ config/env/test.js
@@ -2,8 +2,8 @@ module.exports = {
   mysql: {
     host: 'localhost',
     port: 3306,
-    database: 'shortener_test',
+    database: 'shortener_dev_test',
     username: 'root',
-    password: 'root',
+    password: '',
   },
 };
